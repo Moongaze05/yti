6a5aab911596699b8d3fbcb30b64a0f4de3a49fc
Moongaze05 <makin99@yandex.ru>
2021-06-21 14:40:42 +0500

    news

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,44 +1,40 @@
 <template>
-    <div class="q-pa-md">
+    <div >
     <q-carousel
       animated
       v-model="slide"
       arrows
-      navigation
       infinite
       swipeable
       :autoplay='5000'
+      class="carousel"
     >
       <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
-        <q-card class="card" >
-            <div class="text-h6">
-              Станьте участниками челленджа #ЮИДМЫМОЛОДЫ
-            </div>
-        </q-card>
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Челлендж</div>
+          <div class="text-subtitle1">Станьте участниками челленджа #ЮИДМЫМОЛОДЫ</div>
+        </div>
       </q-carousel-slide>
 
       <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <q-card class="card" >
-            <div class="text-h6">
-              Станьте участниками челленджа #ЮИДМЫМОЛОДЫ
-            </div>
-        </q-card>
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Челлендж</div>
+          <div class="text-subtitle1">Станьте участниками челленджа #ЮИДМЫМОЛОДЫ</div>
+        </div>
       </q-carousel-slide>
 
       <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/mountains.jpg">
-        <q-card class="card" >
-            <div class="text-h6">
-              Станьте участниками челленджа #ЮИДМЫМОЛОДЫ
-            </div>
-        </q-card>
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Челлендж</div>
+          <div class="text-subtitle1">Станьте участниками челленджа #ЮИДМЫМОЛОДЫ</div>
+        </div>
       </q-carousel-slide>
 
       <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg">
-        <q-card class="card" >
-            <div class="text-h6">
-              Станьте участниками челленджа #ЮИДМЫМОЛОДЫ
-            </div>
-        </q-card>
+        <div class="absolute-bottom custom-caption">
+          <div class="text-h2">Челлендж</div>
+          <div class="text-subtitle1">Станьте участниками челленджа #ЮИДМЫМОЛОДЫ</div>
+        </div>
       </q-carousel-slide>
 
     </q-carousel>
@@ -46,10 +42,26 @@
   </div>
 </template>
 <style scoped>
-.card {
 
-  height:100px;
+.custom-caption {
+  text-align: center;
+  padding: 12px;
+  color: white;
+  background-color: rgba(0, 0, 0, .3);
+  }
 
+.carousel {
+  height:50vh;
+  }
+
+  @media (max-width:764px) {
+    .carousel {
+    height:30vh;
+    }
+    .text-h2 {
+      font-size:32px;
+      font-weight: 600;
+    }
   }
 </style>
 <script>
